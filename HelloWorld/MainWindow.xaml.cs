6ba2b421fd098e51bfecdb520bb82899f67ce6ad
Patrice Briggs <PBriggs@perkinscoie.com>
2021-04-29 10:47:37 -0700

    Initial Commit HW 1

---
+++ HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //WindowState = WindowState.Maximized;
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password:" + uxPassword.Text);
+        }
+
+
+        private void uxName_UxPassword_CheckForValues(object sender, TextChangedEventArgs e)
+        {
+            // Enable Submit button if Name and Password have values
+            if (uxName.Text.Length > 0 && uxPassword.Text.Length > 0)           
+                uxSubmit.IsEnabled = true;
+            else
+                uxSubmit.IsEnabled = false;
+        }
+    }
+}
